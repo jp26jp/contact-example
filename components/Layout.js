d736b7d41e8f93d72d57b383e87fc458306a27de
jp26jp <rUNIT026>
2018-10-08 14:48:18 -0400

    Simplified folder structure by moving Nav.js to Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,20 @@
 import Nav from "./Nav"
 import React from "react"
+import Link from "next/link"
 
 const Layout = (props) =>
     <div>
-        <Nav/>
+        <div>
+            <Link href="/contact/John">
+                <a style={linkStyle}>John</a>
+            </Link>
+            <Link href="/contact/Jed">
+                <a style={linkStyle}>Jed</a>
+            </Link>
+            <Link href="/contact/Fred">
+                <a style={linkStyle}>Fred</a>
+            </Link>
+        </div>
         {props.children}
     </div>
 
